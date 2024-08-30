2d968f2b21e633efdfaaf7e556be33a44c739c48
alikonuk1 <alikonuk1@gmail.com>
2024-08-31 00:06:36 +0300

    Change licences

--- src/test/ERC20SB.t.sol
+++ src/test/ERC20SB.t.sol
@@ -10,7 +10,7 @@
                                                            
 */
 
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
